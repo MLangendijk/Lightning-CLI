95cfb9cf0e6555c05507dcb02638af106be43b42
Matthijs <mlangendijk92@hotmail.com>
2022-07-12 14:37:03 +0200

    Remove node modules watch ignore

--- src/actions/watch.js
+++ src/actions/watch.js
@@ -56,7 +56,7 @@ module.exports = (initCallback, watchCallback) => {
         filter(f) {
           return !!!regexp.test(f)
         },
-        ignoreDirectoryPattern: /node_modules|\.git|dist|build/,
+        ignoreDirectoryPattern: /\.git|dist|build/,
       },
       (f, curr, prev) => {
         // prevent initiating another build when already busy
